1fd3538808a61079e9caa7d5899942aec41be292
ahmadabastomi <ahmadabastomi@gmail.com>
2021-01-09 17:48:17 +0700

    fix  bug scrollable on detail movie page

--- lib/ui/pages/movie_detail_page.dart
+++ lib/ui/pages/movie_detail_page.dart
@@ -26,176 +26,182 @@ class MovieDetailPage extends StatelessWidget {
                     return Container(
                       color: accentColor1,
                       child: SafeArea(
-                        child: Container(
-                          color: Color(0xFFE5E5E5),
-                          child: Column(
-                            children: [
-                              //note: Image Header Backdrop
-                              Stack(
-                                children: [
-                                  Container(
-                                    height: 270,
-                                    margin: EdgeInsets.only(bottom: 16),
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: NetworkImage(imageBaseURL +
-                                                "w780" +
-                                                movieModels.backdropPath),
-                                            fit: BoxFit.cover)),
-                                    child: Container(
+                        child: ListView(children: [
+                          Container(
+                            color: Color(0xFFE5E5E5),
+                            child: Column(
+                              children: [
+                                //note: Image Header Backdrop
+                                Stack(
+                                  children: [
+                                    Container(
                                       height: 270,
+                                      margin: EdgeInsets.only(bottom: 16),
                                       decoration: BoxDecoration(
-                                          border: Border(
-                                              bottom: BorderSide(
-                                                  width: 1.0,
-                                                  color: Colors.white)),
-                                          gradient: LinearGradient(
-                                              begin: Alignment.bottomCenter,
-                                              end: Alignment.center,
-                                              colors: [
-                                                Color(0xFFE5E5E5),
-                                                Color(0xFFE5E5E5).withOpacity(0)
-                                              ])),
-                                    ),
-                                  ),
-                                  Container(
-                                    alignment: Alignment.topLeft,
-                                    margin: EdgeInsets.only(top: 20, left: 28),
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        context
-                                            .read<PageBloc>()
-                                            .add(GoToMainPage());
-                                      },
-                                      child: Icon(
-                                        Icons.arrow_back,
-                                        color: Colors.white,
+                                          image: DecorationImage(
+                                              image: NetworkImage(imageBaseURL +
+                                                  "w780" +
+                                                  movieModels.backdropPath),
+                                              fit: BoxFit.cover)),
+                                      child: Container(
+                                        height: 270,
+                                        decoration: BoxDecoration(
+                                            border: Border(
+                                                bottom: BorderSide(
+                                                    width: 1.0,
+                                                    color: Colors.white)),
+                                            gradient: LinearGradient(
+                                                begin: Alignment.bottomCenter,
+                                                end: Alignment.center,
+                                                colors: [
+                                                  Color(0xFFE5E5E5),
+                                                  Color(0xFFE5E5E5)
+                                                      .withOpacity(0)
+                                                ])),
                                       ),
                                     ),
-                                  )
-                                ],
-                              ),
-                              //note: Movie Text Title
-                              Center(
-                                child: Text(
-                                  movieModels.title,
-                                  style: blackTextFont.copyWith(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w600),
+                                    Container(
+                                      alignment: Alignment.topLeft,
+                                      margin:
+                                          EdgeInsets.only(top: 20, left: 28),
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          context
+                                              .read<PageBloc>()
+                                              .add(GoToMainPage());
+                                        },
+                                        child: Icon(
+                                          Icons.arrow_back,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 ),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              //note: Movie Genre
-                              Center(
-                                child: Text(
-                                  movieDetailModels.genresLanguage,
-                                  style: greyTextFont.copyWith(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 6,
-                              ),
-                              //note: Rating Start Vote Average
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  RatingStarts(
-                                    voteAverage: movieModels.voteAverage,
-                                    labelColor: Color(0xFFADADAD),
-                                  )
-                                ],
-                              ),
-                              //note: Cast & Crew Title
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                margin: EdgeInsets.fromLTRB(
-                                    defaultMargin.toDouble(),
-                                    24,
-                                    defaultMargin.toDouble(),
-                                    12),
-                                child: Text(
-                                  'Cast & Crew',
-                                  style: blackTextFont.copyWith(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
+                                //note: Movie Text Title
+                                Center(
+                                  child: Text(
+                                    movieModels.title,
+                                    style: blackTextFont.copyWith(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600),
+                                    textAlign: TextAlign.center,
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                height: 110,
-                                child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: creditModels.length,
-                                  itemBuilder: (_, index) => Container(
-                                    margin: EdgeInsets.only(
-                                        left: (index == 0)
-                                            ? defaultMargin.toDouble()
-                                            : 0,
-                                        right:
-                                            (index == creditModels.length - 1)
-                                                ? defaultMargin.toDouble()
-                                                : 16),
-                                    child: CreditCard(creditModels[index]),
+                                SizedBox(
+                                  height: 6,
+                                ),
+                                //note: Movie Genre
+                                Center(
+                                  child: Text(
+                                    movieDetailModels.genresLanguage,
+                                    style: greyTextFont.copyWith(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w300),
                                   ),
                                 ),
-                              ),
-                              //note: Storyline Movie Text
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                margin: EdgeInsets.only(
-                                    top: 24,
-                                    left: defaultMargin.toDouble(),
-                                    bottom: 8,
-                                    right: defaultMargin.toDouble()),
-                                child: Text(
-                                  'Storyline',
-                                  style: blackTextFont.copyWith(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold),
+                                SizedBox(
+                                  height: 6,
                                 ),
-                              ),
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                margin: EdgeInsets.only(
-                                    left: defaultMargin.toDouble(),
-                                    right: defaultMargin.toDouble(),
-                                    bottom: 30),
-                                child: Text(
-                                  movieModels.overview,
-                                  style: greyTextFont.copyWith(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300),
+                                //note: Rating Start Vote Average
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    RatingStarts(
+                                      voteAverage: movieModels.voteAverage,
+                                      labelColor: Color(0xFFADADAD),
+                                    )
+                                  ],
                                 ),
-                              ),
-                              //note: Button Continue to Book
-                              Center(
-                                child: Container(
-                                  width: 250,
-                                  height: 45,
-                                  child: RaisedButton(
-                                    onPressed: () {},
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(8)),
-                                    color: mainColor,
-                                    child: Text(
-                                      'Continue to Book',
-                                      style: whiteTextFont.copyWith(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w300),
+                                //note: Cast & Crew Title
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  margin: EdgeInsets.fromLTRB(
+                                      defaultMargin.toDouble(),
+                                      24,
+                                      defaultMargin.toDouble(),
+                                      12),
+                                  child: Text(
+                                    'Cast & Crew',
+                                    style: blackTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                height: 40,
-                              )
-                            ],
+                                SizedBox(
+                                  height: 110,
+                                  child: ListView.builder(
+                                    scrollDirection: Axis.horizontal,
+                                    itemCount: creditModels.length,
+                                    itemBuilder: (_, index) => Container(
+                                      margin: EdgeInsets.only(
+                                          left: (index == 0)
+                                              ? defaultMargin.toDouble()
+                                              : 0,
+                                          right:
+                                              (index == creditModels.length - 1)
+                                                  ? defaultMargin.toDouble()
+                                                  : 16),
+                                      child: CreditCard(creditModels[index]),
+                                    ),
+                                  ),
+                                ),
+                                //note: Storyline Movie Text
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  margin: EdgeInsets.only(
+                                      top: 24,
+                                      left: defaultMargin.toDouble(),
+                                      bottom: 8,
+                                      right: defaultMargin.toDouble()),
+                                  child: Text(
+                                    'Storyline',
+                                    style: blackTextFont.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  margin: EdgeInsets.only(
+                                      left: defaultMargin.toDouble(),
+                                      right: defaultMargin.toDouble(),
+                                      bottom: 30),
+                                  child: Text(
+                                    movieModels.overview,
+                                    style: greyTextFont.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                //note: Button Continue to Book
+                                Center(
+                                  child: Container(
+                                    width: 250,
+                                    height: 45,
+                                    child: RaisedButton(
+                                      onPressed: () {},
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8)),
+                                      color: mainColor,
+                                      child: Text(
+                                        'Continue to Book',
+                                        style: whiteTextFont.copyWith(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 40,
+                                )
+                              ],
+                            ),
                           ),
-                        ),
+                        ]),
                       ),
                     );
                   } else {
